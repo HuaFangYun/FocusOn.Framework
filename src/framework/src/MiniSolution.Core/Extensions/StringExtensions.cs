402976601ef2ebe054fdb3dc8d6e111233bb8e62
Zhou Zihao <william-zzh@outlook.com>
2022-06-21 15:19:51 +0800

    补充Core的注释

--- src/framework/src/MiniSolution.Core/Extensions/StringExtensions.cs
+++ src/framework/src/MiniSolution.Core/Extensions/StringExtensions.cs
@@ -1,16 +1,41 @@
 ﻿namespace MiniSolution;
 
+/// <summary>
+/// 字符串扩展类。
+/// </summary>
 public static class StringExtensions
 {
+    /// <summary>
+    /// 判断当前字符串是 <c>null</c> 还是空字符串。
+    /// </summary>
+    /// <param name="value">当前字符串。</param>
+    /// <returns>若为 <c>null</c> 或空字符串则返回 <c>true</c>；否则返回 <c>false</c>。</returns>
     public static bool IsNullOrEmpty(this string value)
         => string.IsNullOrEmpty(value);
 
+    /// <summary>
+    /// 判断当前字符串是 <c>null</c> 、空字符串或空白字符串。
+    /// </summary>
+    /// <param name="value">当前字符串。</param>
+    /// <returns>若为 <c>null</c> 、空字符串或空白字符串则返回 <c>true</c>；否则返回 <c>false</c>。</returns>
     public static bool IsNullOrWhiteSpace(this string value)
         => string.IsNullOrWhiteSpace(value);
 
+    /// <summary>
+    /// 使用指定数组的参数值替换当前字符串的格式化占位字符。
+    /// </summary>
+    /// <param name="value">当前字符串。</param>
+    /// <param name="args">要替换格式化占位的值。</param>
+    /// <returns>替换后的字符串。</returns>
     public static string StringFormat(this string value, params object[] args)
         => string.Format(value, args);
 
+    /// <summary>
+    /// 使用指定的分隔符，将当前数组对象进行=连接。
+    /// </summary>
+    /// <param name="value">要连接的对象数组。</param>
+    /// <param name="seperator">分隔符字符串。</param>
+    /// <returns>数组使用分隔符进行连接过后的字符串。</returns>
     public static string Join(this object[] value, string seperator)
         => string.Join(seperator, value);
 }
--- src/framework/src/MiniSolution.Core/MiniSolutionDependencyInjectionExtensions.cs
+++ src/framework/src/MiniSolution.Core/MiniSolutionDependencyInjectionExtensions.cs
@@ -3,11 +3,25 @@ using MiniSolution;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+/// 对于 <see cref="IServiceCollection"/> 服务注册的扩展。
+/// </summary>
 public static class MiniSolutionDependencyInjectionExtensions
 {
-    public static IServiceCollection AddMiniSolution(this IServiceCollection services,Action<MiniSolutionBuilder> configure)
+    /// <summary>
+    /// 添加 MiniSolution 的指定配置到 <see cref="IServiceCollection"/> 中。
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> 实例。</param>
+    /// <param name="configure">一个可以配置 <see cref="MiniSolutionBuilder"/> 的委托。</param>
+    /// <exception cref="ArgumentNullException"><paramref name="configure"/> 是 <c>null</c>。</exception>
+    public static IServiceCollection AddMiniSolution(this IServiceCollection services, Action<MiniSolutionBuilder> configure)
     {
-        configure( new MiniSolutionBuilder(services));
+        if (configure is null)
+        {
+            throw new ArgumentNullException(nameof(configure));
+        }
+
+        configure(new MiniSolutionBuilder(services));
         return services;
     }
 }

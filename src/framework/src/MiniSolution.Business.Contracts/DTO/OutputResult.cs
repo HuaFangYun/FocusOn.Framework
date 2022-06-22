3cea84d9fcd8b7c07064cbbdbd28daf0a80a19f1
Zhou Zihao <william-zzh@outlook.com>
2022-06-22 10:23:35 +0800

    补全注释

--- src/framework/src/MiniSolution.Business.Contracts/DTO/OutputResult.cs
+++ src/framework/src/MiniSolution.Business.Contracts/DTO/OutputResult.cs
@@ -7,7 +7,7 @@ namespace MiniSolution.Business.Contracts.DTO;
 /// 表示输出返回结果。
 /// </summary>
 [Serializable]
-public class OutputResult
+public record class OutputResult
 {
     /// <summary>
     /// 初始化 <see cref="OutputResult"/> 类的新实例。
@@ -18,19 +18,47 @@ public class OutputResult
         Errors = errors ?? Array.Empty<string?>();
     }
 
+    
+    /// <summary>
+    /// 获取返回的错误信息数组。
+    /// </summary>
     public IEnumerable<string?> Errors { get;private set; } = Array.Empty<string?>();
 
+    /// <summary>
+    /// 获取一个布尔值，表示返回结果是否成功。
+    /// </summary>
     public virtual bool Succeed => !Errors.Any();
 
-    public static OutputResult Success() => new (default);
+    /// <summary>
+    /// 表示操作结果是成功的。
+    /// </summary>
+    public static OutputResult Success() => new (Array.Empty<string?>());
+    /// <summary>
+    /// 表示操作结果是失败的。
+    /// </summary>
+    /// <param name="errors">操作失败的错误信息数组。</param>
     public static OutputResult Failed(params string[] errors) => new(errors);
+    /// <summary>
+    /// 表示操作结果是失败的。
+    /// </summary>
+    /// <param name="errors">操作失败的错误信息数组。</param>
     public static OutputResult Failed(IEnumerable<string?>? errors) => new(errors);
 
+    /// <summary>
+    /// 表示操作结果是失败的。
+    /// </summary>
+    /// <param name="logger"><see cref="ILogger"/> 实例。</param>
+    /// <param name="errors">操作失败的错误信息数组。</param>
     public static OutputResult Failed(ILogger logger, params string[] errors)
     {
         logger.LogError(errors.Join(";"));
         return Failed(errors);
     }
+    /// <summary>
+    /// 表示操作结果是失败的，并记录异常日志。
+    /// </summary>
+    /// <param name="logger"><see cref="ILogger"/> 实例。</param>
+    /// <param name="ex">要记录的异常。</param>
     public static OutputResult Failed(ILogger logger,Exception ex)
     {
         logger.LogError(ex, ex.Message);
@@ -38,25 +66,54 @@ public class OutputResult
     }
 }
 
+/// <summary>
+/// 表示具有返回值 <typeparamref name="TResult"/> 的输出返回结果。
+/// </summary>
+/// <typeparam name="TResult">返回值的类型。</typeparam>
 [Serializable]
-public class OutputResult<TResult> : OutputResult
+public record class OutputResult<TResult> : OutputResult
 {
-    public OutputResult(TResult? data, IEnumerable<string>? errors) :base(errors)
-    {
-        Data = data;
-    }
+    /// <summary>
+    /// 初始化 <see cref="OutputResult{TResult}"/> 类的新实例。
+    /// </summary>
+    /// <param name="data">要返回的数据。</param>
+    /// <param name="errors">错误信息数组。</param>
+    public OutputResult(TResult? data, IEnumerable<string>? errors) : base(errors) => Data = data;
 
-
+    /// <summary>
+    /// 获取执行结果成功后的返回数据。
+    /// </summary>
     public TResult? Data { get; }
 
+    /// <summary>
+    /// 表示操作结果是成功的，并设置返回的数据。
+    /// </summary>
     public static OutputResult<TResult> Success(TResult data) => new(data, null);
+    /// <summary>
+    /// 表示操作结果是失败的。
+    /// </summary>
+    /// <param name="errors">操作失败的错误信息数组。</param>
     public static new OutputResult<TResult> Failed(params string[] errors) => new(default, errors);
+    /// <summary>
+    /// 表示操作结果是失败的。
+    /// </summary>
+    /// <param name="errors">操作失败的错误信息数组。</param>
     public static new OutputResult<TResult> Failed(IEnumerable<string>? errors) => new(default, errors);
+    /// <summary>
+    /// 表示操作结果是失败的。
+    /// </summary>
+    /// <param name="logger"><see cref="ILogger"/> 实例。</param>
+    /// <param name="errors">操作失败的错误信息数组。</param>
     public static new OutputResult<TResult> Failed(ILogger logger, params string[] errors)
     {
         logger.LogError(errors.Join(";"));
         return Failed(errors);
     }
+    /// <summary>
+    /// 表示操作结果是失败的，并记录异常日志。
+    /// </summary>
+    /// <param name="logger"><see cref="ILogger"/> 实例。</param>
+    /// <param name="ex">要记录的异常。</param>
     public static new OutputResult<TResult> Failed(ILogger logger, Exception ex)
     {
         logger.LogError(ex, ex.Message);

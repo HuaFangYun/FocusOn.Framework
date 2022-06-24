708f13099938412c4852d131712f4566537bca5c
Zhou Zihao <william-zzh@outlook.com>
2022-06-24 10:01:21 +0800

    增加 RemotingServiceAttribute 特性

---
+++ src/framework/src/MiniSolution.Business.Contracts/RemotingServiceAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniSolution.Business.Contracts;
+[AttributeUsage(AttributeTargets.Class| AttributeTargets.Interface| AttributeTargets.Method,AllowMultiple =true)]
+public sealed class RemotingServiceAttribute : Attribute
+{
+    public RemotingServiceAttribute(string? template=default)
+    {
+        Template = template;
+    }
+
+    public string Template { get; }
+    public bool Disabled { get; set; }
+}

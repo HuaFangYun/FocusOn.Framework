58a14e986bcaff93cd16c46646424c4265f6e791
William <william-zzh@outlook.com>
2022-07-08 23:40:14 +0800

    提交注释

--- src/framework/src/FocusOn.Framework.Endpoint.HttpApi/Controllers/EfCoreApiControllerBase.cs
+++ src/framework/src/FocusOn.Framework.Endpoint.HttpApi/Controllers/EfCoreApiControllerBase.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FocusOn.Framework.Endpoint.HttpApi.Controllers;
 
+/// <summary>
+/// 基于 EF Core 的 HTTP API 的基类。
+/// </summary>
+/// <typeparam name="TContext">数据库上下文。</typeparam>
+/// <typeparam name="TEntity">实体类型。</typeparam>
+/// <typeparam name="TKey">主键类型。</typeparam>
 public abstract class EfCoreApiControllerBase<TContext, TEntity, TKey> : ApiControllerBase
     where TContext : DbContext
     where TEntity : class
     where TKey : IEquatable<TKey>
 {
+    /// <summary>
+    /// 初始化 <see cref="EfCoreApiControllerBase{TContext, TEntity, TKey}"/> 类的新实例。
+    /// </summary>
     protected EfCoreApiControllerBase(IServiceProvider serviceProvider) : base(serviceProvider)
     {
     }

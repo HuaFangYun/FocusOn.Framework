b9f3b6d13a00f98027b06978bbdd5e7cbf685796
William <william-zzh@outlook.com>
2022-07-13 00:50:42 +0800

    增加创建时间的配置

--- src/framework/src/FocusOn.Framework.Business.Store/Configurations/EntityTypeBuilderExtensions.cs
+++ src/framework/src/FocusOn.Framework.Business.Store/Configurations/EntityTypeBuilderExtensions.cs
@@ -11,28 +11,22 @@ namespace FocusOn.Framework.Business.Store.Configurations;
 /// </summary>
 public static class EntityTypeBuilderExtensions
 {
+    public static EntityTypeBuilder<TEntity> ConfigureConvensions<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : class
+        => builder.TryConfigureMultiTenancy()
+                 .TryConfigureCreateTime();
+
     /// <summary>
     /// 配置 Id 作为主键。
     /// </summary>
     /// <typeparam name="TEntity">继承 <see cref="EntityBase{TKey}"/> 类的实体类型。</typeparam>
     /// <typeparam name="TKey">主键类型。</typeparam>
     /// <param name="builder"><see cref="EntityTypeBuilder{TEntity}"/> 实例。</param>
-    public static EntityTypeBuilder<TEntity> ConfigureKey<TEntity, TKey>(this EntityTypeBuilder<TEntity> builder) 
-        where TEntity :class, IHasId<TKey>
-        where TKey:IEquatable<TKey>
+    public static EntityTypeBuilder<TEntity> ConfigureKey<TEntity, TKey>(this EntityTypeBuilder<TEntity> builder)
+        where TEntity : class, IHasId<TKey>
+        where TKey : IEquatable<TKey>
     {
         builder.HasKey(m => m.Id);
         builder.Property(m => m.Id).ValueGeneratedOnAdd();
-        return builder;
-    }
-    /// <summary>
-    /// 配置多租户字段。
-    /// </summary>
-    /// <typeparam name="TEntity">实体类型。</typeparam>
-    /// <param name="builder"><see cref="EntityTypeBuilder{TEntity}"/> 实例。</param>
-    public static EntityTypeBuilder<TEntity> ConfigureMultiTenancy<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : class, IHasMultiTenancy
-    {
-        builder.TryConfigureMultiTenancy();
         return builder;
     }
 
@@ -41,14 +35,27 @@ public static class EntityTypeBuilderExtensions
     /// </summary>
     /// <typeparam name="TEntity">实体类型。</typeparam>
     /// <param name="builder"><see cref="EntityTypeBuilder{TEntity}"/> 实例。</param>
-    public static bool TryConfigureMultiTenancy<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : class
+    public static EntityTypeBuilder<TEntity> TryConfigureMultiTenancy<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : class
     {
         if (builder.Metadata is IHasMultiTenancy multiTenancy)
         {
             builder.Property(nameof(IHasMultiTenancy.TenantId));
             builder.HasQueryFilter(m => EF.Property<Guid>(m, nameof(IHasMultiTenancy.TenantId)) == multiTenancy.TenantId);
-            return true;
         }
-        return false;
+        return builder;
+    }
+
+    /// <summary>
+    /// 尝试配置创建时间字段。
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型。</typeparam>
+    /// <param name="builder"><see cref="EntityTypeBuilder{TEntity}"/> 实例。</param>
+    public static EntityTypeBuilder<TEntity> TryConfigureCreateTime<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : class
+    {
+        if (builder.Metadata is IHasCreateTime createTime)
+        {
+            builder.Property(nameof(IHasCreateTime.CreateTime));
+        }
+        return builder;
     }
 }
